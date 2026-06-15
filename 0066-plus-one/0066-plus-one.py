8eb8df35ff9b85a58d7ae0e99904bcc968aacd37
Mohan Raj <mohan.ins.nvm@gmail.com>
2026-06-15 23:27:18 +0530

    Time: 0 ms (100%), Space: 19.3 MB (57.37%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,11 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for num in range(len(digits)-1,-1,-1):
+            if digits[num]<9:
+                digits[num]+=1
+                return digits
+            digits[num]=0
+
+        return [1]+ digits
+        
+        
