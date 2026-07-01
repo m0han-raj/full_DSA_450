e5e3e04560b90ce3d551271e0979a9e26f65e8c5
Mohan Raj <mohan.ins.nvm@gmail.com>
2026-07-01 14:34:38 +0530

    Time: 42 ms (56.82%), Space: 23.6 MB (68.13%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        stk =[]
+
+        for c in s :
+            if stk and stk[-1][0] == c:
+                stk[-1][1]=(stk[-1][1]+1)%k
+                if stk[-1][1] == 0:
+                    stk.pop()
+            else:
+                stk.append([c,1])
+        result = [c*v for c,v in stk]
+        return "".join(result)
